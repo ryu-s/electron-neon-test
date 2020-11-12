5623af3f8607f64dedc4654fa11f3ad32bc84fc4
Ryu Shinmura <rysestock@gmail.com>
2020-11-12 12:12:28 +0900

    remove script 'install'

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function createWindow() {
     }
   })
 
-  //console.log(addon.hello())
+  console.log(addon.hello())
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
